30d21826e96f5ab7a72051d424c21e1b7da48a6f
Hamza Ramzanali <hamzaramzanali@Hamzas-MacBook-Air.local>
2022-09-13 10:17:42 -0500

    added changes to java and html

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,7 @@ var formEl = document.querySelector('#form')
 var textinputEl = document.querySelector('#cityinput')
 var appID = "a5e8b6f890ce08796c7125320b836ba0"
 var historyList = JSON.parse(localStorage.getItem("history")) || []
+var searchHistoryEl = document.querySelector('#search-history')
 
 formEl.addEventListener("submit", function(event) {
     event.preventDefault()
@@ -28,7 +29,7 @@ fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${usercity}&limt=1&appid=$
 
 function getWeather(lat,lon){
     console.log(lat,lon,appID)
-    fetch(`http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${appID}`)
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${appID}`)
     .then(function (response) {
         return response.json();
     })
@@ -41,4 +42,28 @@ function saveLocalStorage(name){
     historyList.push(name)
     localStorage.setItem('history', JSON.stringify(historyList))
 }
-    localStorage.getItem(historyList)
+    
+function getSearchHistory(){
+    let searchHistory = localstorage.getItem('history')
+    if (searchHistory) {
+        historyList = JSON.parse(searchHistory)
+    }
+    renderSearchHistory()
+}
+
+function renderSearchHistory(){
+    searchHistoryEl.innerHTML = ""
+    for (var i = 0; i < historyList.length; i++){
+        var btn = document.createElement('button')
+        btn.setAttribute('type', 'button')
+        btn.setAttribute("data-search", historyList[i])
+        btn.classList.add('btn-history')
+        btn.textContent = historyList[i]
+        searchHistoryEl.append(btn)
+    }
+}
+
+// function handleSearchHistory(e){
+//     e.preventDefault()
+//     if (!e.target.matches(b))
+// }
